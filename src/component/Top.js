115e1597b3eee75d79128544f675e9c3d6ec839f
Salvation_sub <kyman19940214@gmail.com>
2021-06-14 18:07:31 +0900

    First Commit

---
+++ src/component/Top.js
@@ -0,0 +1,11 @@
+import Gnb from "./Gnb";
+
+export default function Top() {
+  return (
+    <div>
+      <img src="/images/Ahri_16.jpg" alt="log"></img>
+      <div as="h1">넥스트 TEST</div>
+      <Gnb />
+    </div>
+  );
+}
